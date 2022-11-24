c39d5961c53e2b0505f7d31b4af7febfc02c8823
s3limm <108307150+s3limm@users.noreply.github.com>
2022-11-24 18:32:22 +0300

    learned how to get data from user

---
+++ GettingData/GettingData/Models/Products.cs
@@ -0,0 +1,8 @@
+﻿namespace GettingData.Models
+{
+    public class Products
+    {
+        public int ID { get; set; }
+        public string productName { get; set; }
+    }
+}
--- GettingData/GettingData/Startup.cs
+++ GettingData/GettingData/Startup.cs
@@ -16,6 +16,7 @@ namespace GettingData
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +31,16 @@ namespace GettingData
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("CustomRoute", "{controller=Home}/{action=Index}/{ID?}/{productName?}");
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    //endpoints.MapDefaultControllerRoute();
+
+
+
+                    
+
+                //});
             });
         }
     }
