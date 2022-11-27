50a8443223318490814e041a0c668844e29362cd
sn3al <108307150+s3limm@users.noreply.github.com>
2022-11-27 19:34:36 +0300

    learned TagHelpers

---
+++ GettingData/GettingData/Controllers/TeacherController.cs
@@ -0,0 +1,30 @@
+﻿using GettingData.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GettingData.Controllers
+{
+    public class TeacherController : Controller
+    {
+        List<Branches> branches= new List<Branches>()
+        { 
+        new Branches() {ID=1,BranchName="Matematik"},
+        new Branches() {ID=2,BranchName="Edebiyat"},
+        new Branches() {ID=3,BranchName="Tarih"},
+        new Branches() {ID=4,BranchName="Coğrafya"},
+        };
+
+        public IActionResult AddTeacher()
+        {
+            ViewBag.Branches = branches;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddTeacher(Teacher teacher)
+        {
+            ViewBag.Branches = branches;
+            return View();
+        }
+    }
+}
---
+++ GettingData/GettingData/Models/Teacher.cs
@@ -0,0 +1,9 @@
+﻿namespace GettingData.Models
+{
+    public class Teacher
+    {
+        public string FirstName{ get; set; }
+        public string LastName { get; set; }
+        public int BranchesID { get; set; }
+    }
+}
